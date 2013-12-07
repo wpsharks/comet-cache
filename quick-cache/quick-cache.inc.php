402fd1a80309bfed89cb2d408cd805e693dfa708
Raam Dev <raam@websharks-inc.com>
2013-12-07 18:28:29 -0500

    Remove depreciated VHOST code that was creating a bug with cache clearing on WPMU.

--- quick-cache/quick-cache.inc.php
+++ quick-cache/quick-cache.inc.php
@@ -340,7 +340,7 @@ namespace quick_cache // Root namespace.
 							$http_host_nps  = preg_replace('/\:[0-9]+$/', '', $_SERVER['HTTP_HOST']);
 							$host_dir_token = '/'; // Assume NOT multisite; or running it's own domain.
 
-							if($is_multisite && (!defined('SUBDOMAIN_INSTALL') || !SUBDOMAIN_INSTALL) && (!defined('VHOST') || !VHOST))
+							if($is_multisite && (!defined('SUBDOMAIN_INSTALL') || !SUBDOMAIN_INSTALL))
 								{ // Multisite w/ sub-directories; need sub-directory. We MUST validate against blog paths too.
 
 									list($host_dir_token) = explode('/', trim($_SERVER['REQUEST_URI'], '/'));
