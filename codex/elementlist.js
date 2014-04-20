f54c3b9284088bbdfc317bb167584d9b919c894d
JasWSInc <jason@websharks-inc.com>
2014-04-20 01:32:42 -0800

    Adding codex files.

--- codex/elementlist.js
+++ codex/elementlist.js
@@ -1,3 +1,3 @@
 
 var ApiGen = ApiGen || {};
-ApiGen.elements = [["f","__()"],["f","plugin()"],["c","quick_cache\\actions"],["c","quick_cache\\advanced_cache"],["c","quick_cache\\menu_pages"],["c","quick_cache\\plugin"],["f","quick_cache_php53_dashboard_notice()"]];
+ApiGen.elements = [["f","__()"],["f","plugin()"],["c","quick_cache\\actions"],["c","quick_cache\\advanced_cache"],["c","quick_cache\\menu_pages"],["c","quick_cache\\plugin"],["f","quick_cache_php53_dashboard_notice()"],["f","wp_cache_postload()"]];
