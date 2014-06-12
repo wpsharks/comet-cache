0372e8d889c6a7787d3b7c02cd35f616cf630e6a
JasWSInc <jason@websharks-inc.com>
2014-06-12 12:50:51 -0800

    Adding class_exists check. https://github.com/websharks/quick-cache/issues/151

--- quick-cache/includes/share.php
+++ quick-cache/includes/share.php
@@ -4,19 +4,22 @@ namespace quick_cache // Root namespace.
 	if(!defined('WPINC')) // MUST have WordPress.
 		exit('Do NOT access this file directly: '.basename(__FILE__));
 
-	/**
-	 * Quick Cache (Shared Methods)
-	 *
-	 * @package quick_cache\share
-	 * @since 14xxxx Reorganizing class members.
-	 */
-	class share // Shared between {@link advanced_cache} and {@link plugin}.
+	if(!class_exists('\\'.__NAMESPACE__.'\\share'))
 	{
-		public function __construct()
+		/**
+		 * Quick Cache (Shared Methods)
+		 *
+		 * @package quick_cache\share
+		 * @since 14xxxx Reorganizing class members.
+		 */
+		class share // Shared between {@link advanced_cache} and {@link plugin}.
 		{
-			// @TODO Bring shareable methods into this class.
+			public function __construct()
+			{
+				// @TODO Bring shareable methods into this class.
+			}
 		}
-	}
 
-	$GLOBALS[__NAMESPACE__.'__share'] = new share();
+		$GLOBALS[__NAMESPACE__.'__share'] = new share();
+	}
 }
