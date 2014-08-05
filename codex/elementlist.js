3918b0aa8bb8c50e6e6e51e5288460e2c55f0a1b
Raam Dev <raam@raamdev.com>
2014-08-04 20:40:09 -0400

    Update Codex for v140725

--- codex/elementlist.js
+++ codex/elementlist.js
@@ -1,3 +1,3 @@
 
 var ApiGen = ApiGen || {};
-ApiGen.elements = [["f","__()"],["f","plugin()"],["c","quick_cache"],["c","quick_cache\\actions"],["c","quick_cache\\advanced_cache"],["c","quick_cache\\menu_pages"],["c","quick_cache\\plugin"],["f","quick_cache_php53_dashboard_notice()"],["f","wp_cache_postload()"]];
+ApiGen.elements = [["f","__()"],["f","plugin()"],["c","quick_cache"],["c","quick_cache\\actions"],["c","quick_cache\\advanced_cache"],["c","quick_cache\\menu_pages"],["c","quick_cache\\plugin"],["c","quick_cache\\share"],["c","quick_cache\\version_specific_upgrade"],["f","wp_cache_postload()"],["f","wp_php53()"],["f","wp_php53_custom_notice()"],["f","wp_php53_notice()"]];
