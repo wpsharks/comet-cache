2222830ef515af8a6ec42e69f91acea609e2756d
Raam Dev <raam@websharks-inc.com>
2014-11-10 23:16:06 -0500

    Update Codex for v141110

--- codex/elementlist.js
+++ codex/elementlist.js
@@ -1,3 +1,3 @@
 
 var ApiGen = ApiGen || {};
-ApiGen.elements = [["f","__()"],["f","plugin()"],["c","quick_cache"],["c","quick_cache\\actions"],["c","quick_cache\\advanced_cache"],["c","quick_cache\\menu_pages"],["c","quick_cache\\plugin"],["c","quick_cache\\share"],["c","quick_cache\\uninstall"],["c","quick_cache\\version_specific_upgrade"],["f","wp_cache_postload()"],["f","wp_php53()"],["f","wp_php53_custom_notice()"],["f","wp_php53_notice()"]];
+ApiGen.elements = [["f","__()"],["f","plugin()"],["c","quick_cache"],["c","quick_cache\\actions"],["c","quick_cache\\advanced_cache"],["c","quick_cache\\menu_pages"],["c","quick_cache\\plugin"],["c","quick_cache\\share"],["c","quick_cache\\uninstall"],["c","quick_cache\\utils_feed"],["c","quick_cache\\version_specific_upgrade"],["f","wp_cache_postload()"],["f","wp_php53()"],["f","wp_php53_custom_notice()"],["f","wp_php53_notice()"]];
