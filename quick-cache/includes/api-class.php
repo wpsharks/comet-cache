edfe30654a823880da478f25165a996cd9e48769
JasWSInc <jas@wsharks.com>
2014-11-19 11:51:03 -0900

    Adding `class_exists()` check to be sure to avoid any conflicts in ZenCache; if someone happens to enable both. Ugh. See: websharks/zencache#5

--- quick-cache/includes/api-class.php
+++ quick-cache/includes/api-class.php
@@ -12,79 +12,82 @@ namespace // Global namespace.
 	if(!defined('WPINC')) // MUST have WordPress.
 		exit('Do NOT access this file directly: '.basename(__FILE__));
 
-	/**
-	 * Quick Cache API Class.
-	 */
-	class quick_cache
+	if(!class_exists('\\quick_cache'))
 	{
 		/**
-		 * Current QC plugin instance.
-		 *
-		 * @return \quick_cache\plugin instance.
+		 * Quick Cache API Class.
 		 */
-		public static function plugin()
+		class quick_cache
 		{
-			return $GLOBALS[__CLASS__];
-		}
+			/**
+			 * Current QC plugin instance.
+			 *
+			 * @return \quick_cache\plugin instance.
+			 */
+			public static function plugin()
+			{
+				return $GLOBALS[__CLASS__];
+			}
 
-		/**
-		 * Gives you the current version string.
-		 *
-		 * @return string Current version string.
-		 */
-		public static function version()
-		{
-			return static::plugin()->version;
-		}
+			/**
+			 * Gives you the current version string.
+			 *
+			 * @return string Current version string.
+			 */
+			public static function version()
+			{
+				return static::plugin()->version;
+			}
 
-		/**
-		 * Gives you the current array of configured options.
-		 *
-		 * @return array Current array of options.
-		 */
-		public static function options()
-		{
-			return static::plugin()->options;
-		}
+			/**
+			 * Gives you the current array of configured options.
+			 *
+			 * @return array Current array of options.
+			 */
+			public static function options()
+			{
+				return static::plugin()->options;
+			}
 
-		/**
-		 * Purges expired cache files, leaving all others intact.
-		 *
-		 * @note This occurs automatically over time via WP Cron;
-		 *    but this will force an immediate purge if you so desire.
-		 *
-		 * @return integer Total files purged (if any).
-		 */
-		public static function purge()
-		{
-			return static::plugin()->purge_cache();
-		}
+			/**
+			 * Purges expired cache files, leaving all others intact.
+			 *
+			 * @note This occurs automatically over time via WP Cron;
+			 *    but this will force an immediate purge if you so desire.
+			 *
+			 * @return integer Total files purged (if any).
+			 */
+			public static function purge()
+			{
+				return static::plugin()->purge_cache();
+			}
 
-		/**
-		 * This erases the entire cache for the current blog.
-		 *
-		 * @note In a multisite network this impacts only the current blog,
-		 *    it does not clear the cache for other child blogs.
-		 *
-		 * @return integer Total files cleared (if any).
-		 */
-		public static function clear()
-		{
-			return static::plugin()->clear_cache();
-		}
+			/**
+			 * This erases the entire cache for the current blog.
+			 *
+			 * @note In a multisite network this impacts only the current blog,
+			 *    it does not clear the cache for other child blogs.
+			 *
+			 * @return integer Total files cleared (if any).
+			 */
+			public static function clear()
+			{
+				return static::plugin()->clear_cache();
+			}
 
-		/**
-		 * This wipes out the entire cache.
-		 *
-		 * @note On a standard WP installation this is the same as quick_cache::clear();
-		 *    but on a multisite installation it impacts the entire network
-		 *    (i.e. wipes the cache for all blogs in the network).
-		 *
-		 * @return integer Total files wiped (if any).
-		 */
-		public static function wipe()
-		{
-			return static::plugin()->wipe_cache();
+			/**
+			 * This wipes out the entire cache.
+			 *
+			 * @note On a standard WP installation this is the same as quick_cache::clear();
+			 *    but on a multisite installation it impacts the entire network
+			 *    (i.e. wipes the cache for all blogs in the network).
+			 *
+			 * @return integer Total files wiped (if any).
+			 */
+			public static function wipe()
+			{
+				return static::plugin()->wipe_cache();
+			}
 		}
 	}
 }
